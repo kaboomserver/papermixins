7e93974ec2be4a1ace159c27eef03f555d579415
amyavi <144570677+amyavi@users.noreply.github.com>
2025-02-12 01:19:33 -0300

    feat: basic ignite mod structure

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    id("checkstyle")
+    id("java-library")
+    id("io.papermc.paperweight.userdev") version "2.0.0-beta.14"
+}
+
+group = "pw.kaboom"
+version = "master"
+
+repositories {
+    maven("https://repo.papermc.io/repository/maven-public/")
+    maven("https://repo.spongepowered.org/maven/")
+}
+
+dependencies {
+    paperweight.paperDevBundle("1.21.4-R0.1-SNAPSHOT")
+
+    compileOnly("space.vectrix.ignite:ignite-api:1.1.0")
+    compileOnly("org.spongepowered:mixin:0.8.7")
+    compileOnly("io.github.llamalad7:mixinextras-common:0.4.1")
+}
+
+java {
+    toolchain.languageVersion = JavaLanguageVersion.of(21)
+}
