ce8fecceaf575390e32b2a19323116a285b18007
amyavi <144570677+amyavi@users.noreply.github.com>
2025-02-12 01:20:48 -0300

    feat: disable username validation

---
+++ src/main/java/pw/kaboom/papermixins/mixin/disable_username_validation/CraftPlayerProfileMixin.java
@@ -0,0 +1,17 @@
+package pw.kaboom.papermixins.mixin.disable_username_validation;
+
+import com.destroystokyo.paper.profile.CraftPlayerProfile;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(CraftPlayerProfile.class)
+public abstract class CraftPlayerProfileMixin {
+    @WrapOperation(method = "createAuthLibProfile", at = @At(value = "INVOKE",
+            target = "Lnet/minecraft/util/StringUtil;isValidPlayerName(Ljava/lang/String;)Z"))
+    private static boolean createAuthLibProfile$isValidPlayerName(final String playerName,
+                                                                  final Operation<Boolean> original) {
+        return true; // :)
+    }
+}
