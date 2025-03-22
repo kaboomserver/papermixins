1e8cc1b27f7cb87734d6458c630c610822584684
OptimisticDeving <173472493+OptimisticDeving@users.noreply.github.com>
2025-03-22 22:47:59 +0000

    perf: only schedule ticks for command blocks when their command is set/they are powered
    this provides orders of magnitude more server performance when a large amount of automatic repeating command blocks are loaded

---
+++ src/main/java/pw/kaboom/papermixins/mixin/perf/command_block_optimization/CommandBlockEntityBaseCommandBlockImplMixin.java
@@ -0,0 +1,28 @@
+package pw.kaboom.papermixins.mixin.perf.command_block_optimization;
+
+import net.minecraft.util.StringUtil;
+import net.minecraft.world.level.block.entity.CommandBlockEntity;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import pw.kaboom.papermixins.mixin.perf.command_block_optimization.accessor.CommandBlockEntityAccessor;
+
+@Mixin(targets = "net.minecraft.world.level.block.entity.CommandBlockEntity$1")
+public class CommandBlockEntityBaseCommandBlockImplMixin {
+    @Shadow
+    @Final
+    CommandBlockEntity this$0;
+
+    @Inject(method = "setCommand", at = @At("TAIL"))
+    private void onSetCommand(final String command, final CallbackInfo ci) {
+        if (
+                StringUtil.isNullOrEmpty(command)
+                        || (!this$0.isAutomatic() || this$0.getMode() != CommandBlockEntity.Mode.AUTO)
+        )
+            return;
+        ((CommandBlockEntityAccessor) this$0).invokeScheduleTick();
+    }
+}
---
+++ src/main/java/pw/kaboom/papermixins/mixin/perf/command_block_optimization/CommandBlockMixin.java
@@ -0,0 +1,23 @@
+package pw.kaboom.papermixins.mixin.perf.command_block_optimization;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.CommandBlock;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(CommandBlock.class)
+public abstract class CommandBlockMixin {
+    @WrapOperation(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerLevel;" +
+            "scheduleTick(Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/Block;I)V"))
+    private void tick$scheduleTick(final ServerLevel instance, final BlockPos blockPos,
+                                   final Block block, final int delay,
+                                   final Operation<Void> original) {
+        // Don't schedule the tick here
+    }
+}
