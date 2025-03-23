80d415e24eabfa23b020244d846632cea7fe2c60
OptimisticDeving <173472493+OptimisticDeving@users.noreply.github.com>
2025-03-23 01:16:07 +0000

    fix: support lower protocol versions

--- src/main/java/pw/kaboom/papermixins/mixin/feat/encryption/ServerLoginPacketListenerImplMixin.java
+++ src/main/java/pw/kaboom/papermixins/mixin/feat/encryption/ServerLoginPacketListenerImplMixin.java
@@ -3,14 +3,18 @@ package pw.kaboom.papermixins.mixin.feat.encryption;
 import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.core.UUIDUtil;
+import net.minecraft.network.Connection;
 import net.minecraft.network.protocol.login.ClientboundHelloPacket;
+import net.minecraft.network.protocol.login.ServerboundHelloPacket;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerLoginPacketListenerImpl;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import pw.kaboom.papermixins.util.ViaVersionHax;
 
 @Mixin(ServerLoginPacketListenerImpl.class)
 public abstract class ServerLoginPacketListenerImplMixin {
@@ -18,14 +22,20 @@ public abstract class ServerLoginPacketListenerImplMixin {
     @Final
     MinecraftServer server;
 
+    @Shadow
+    @Final
+    public Connection connection;
+
     @WrapOperation(
             method = "handleHello",
             at = @At(
                     value = "INVOKE",
                     target = "Lnet/minecraft/server/MinecraftServer;usesAuthentication()Z"
             ))
-    private boolean handleHello$startClientVerification(final MinecraftServer instance, final Operation<Boolean> original) {
-        return true;
+    private boolean handleHello$usesAuthentication(final MinecraftServer instance, final Operation<Boolean> original,
+                                                   final @Local(argsOnly = true) ServerboundHelloPacket helloPacket) {
+        return original.call(instance)
+                || ViaVersionHax.getOriginalVersion(this.connection.channel) >= 766;
     }
 
     @WrapOperation(
