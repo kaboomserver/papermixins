6e957743893e7d244c2cb9a045c55ddd6333ca8b
amyavi <144570677+amyavi@users.noreply.github.com>
2025-04-01 23:52:13 -0300

    refactor: use ModifyArg for SpigotPluginProviderMixin

--- src/main/java/pw/kaboom/papermixins/mixin/feat/plugin_mixins/SpigotPluginProviderMixin.java
+++ src/main/java/pw/kaboom/papermixins/mixin/feat/plugin_mixins/SpigotPluginProviderMixin.java
@@ -1,57 +1,38 @@
 package pw.kaboom.papermixins.mixin.feat.plugin_mixins;
 
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-import io.papermc.paper.plugin.provider.entrypoint.DependencyContext;
 import io.papermc.paper.plugin.provider.type.spigot.SpigotPluginProvider;
 import org.bukkit.plugin.PluginDescriptionFile;
-import org.bukkit.plugin.java.PluginClassLoader;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import pw.kaboom.papermixins.pluginmixin.PluginMixinLoader;
 import pw.kaboom.papermixins.pluginmixin.PluginMixinSeparationClassLoader;
 import pw.kaboom.papermixins.pluginmixin.interop.LoadedPluginMixin;
 
-import java.io.File;
 import java.util.List;
-import java.util.jar.JarFile;
 
 @Mixin(SpigotPluginProvider.class)
 public abstract class SpigotPluginProviderMixin {
-    @WrapOperation(
-            method = "createInstance()Lorg/bukkit/plugin/java/JavaPlugin;",
-            at = @At(
-                    value = "NEW",
-                    target = "(Ljava/lang/ClassLoader;Lorg/bukkit/plugin/PluginDescriptionFile;" +
-                            "Ljava/io/File;Ljava/io/File;Ljava/lang/ClassLoader;Ljava/util/jar/JarFile;" +
-                            "Lio/papermc/paper/plugin/provider/entrypoint/DependencyContext;)" +
-                            "Lorg/bukkit/plugin/java/PluginClassLoader;"))
-    private PluginClassLoader createInstance$newPluginClassLoader(final ClassLoader parentClassLoader,
-                                                                  final PluginDescriptionFile description,
-                                                                  final File pluginFile,
-                                                                  final File pluginDataFolder,
-                                                                  final ClassLoader libraryClassLoader,
-                                                                  final JarFile jarFile,
-                                                                  final DependencyContext dependencyContext,
-                                                                  final Operation<PluginClassLoader> original) {
-        final List<LoadedPluginMixin> pluginMixins = PluginMixinLoader.getPluginMixins(description.getName());
-        if (pluginMixins == null) {
-            return original.call(parentClassLoader,
-                    description,
-                    pluginFile,
-                    pluginDataFolder,
-                    libraryClassLoader,
-                    jarFile,
-                    dependencyContext);
-        }
+    @Shadow
+    @Final
+    private PluginDescriptionFile description;
 
-        PluginMixinLoader.LOGGER.info("Modifying plugin class loader instantiation for {}", description.getName());
-        return original.call(new PluginMixinSeparationClassLoader(parentClassLoader, pluginMixins),
-                description,
-                pluginFile,
-                pluginDataFolder,
-                libraryClassLoader,
-                jarFile,
-                dependencyContext);
+    @ModifyArg(method = "createInstance()Lorg/bukkit/plugin/java/JavaPlugin;",
+            index = 0,
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lorg/bukkit/plugin/java/PluginClassLoader;<init>(Ljava/lang/ClassLoader;" +
+                            "Lorg/bukkit/plugin/PluginDescriptionFile;Ljava/io/File;Ljava/io/File;" +
+                            "Ljava/lang/ClassLoader;Ljava/util/jar/JarFile;" +
+                            "Lio/papermc/paper/plugin/provider/entrypoint/DependencyContext;)V"))
+    private ClassLoader createInstance$newPluginClassLoader(final ClassLoader parent) {
+        final String pluginName = description.getName();
+        final List<LoadedPluginMixin> pluginMixins = PluginMixinLoader.getPluginMixins(pluginName);
+        if (pluginMixins == null) return parent;
+
+        PluginMixinLoader.LOGGER.info("Modifying plugin class loader instantiation for {}", pluginName);
+        return new PluginMixinSeparationClassLoader(parent, pluginMixins);
     }
 }
