f447cae9b0fbcac354a414f75ccf831f0e5ecd69
amyavi <144570677+amyavi@users.noreply.github.com>
2025-04-12 14:22:35 -0300

    feat: port to 1.21.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ repositories {
 }
 
 dependencies {
-    paperweight.paperDevBundle("1.21.4-R0.1-SNAPSHOT")
+    paperweight.paperDevBundle("1.21.5-R0.1-SNAPSHOT")
 
     compileOnly("space.vectrix.ignite:ignite-api:1.1.0")
     compileOnly(libs.mixin)
