4631dd5d91d4ae39d201bb67b53058a9e5ba1c0f
amyavi <144570677+amyavi@users.noreply.github.com>
2025-04-12 22:13:37 -0300

    fix: also restrict structure blocks

---
+++ src/main/java/pw/kaboom/papermixins/mixin/fix/command_block_restrictions/StructureTemplateMixin.java
@@ -0,0 +1,26 @@
+package pw.kaboom.papermixins.mixin.fix.command_block_restrictions;
+
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.CommandBlockEntity;
+import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplate;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(StructureTemplate.class)
+public abstract class StructureTemplateMixin {
+    @ModifyExpressionValue(method = "fillFromWorld",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/entity/BlockEntity;" +
+                    "saveWithId(Lnet/minecraft/core/HolderLookup$Provider;)Lnet/minecraft/nbt/CompoundTag;"))
+    private CompoundTag fillFromWorld$saveWithId(final CompoundTag original,
+                                                 final @Local(ordinal = 0) BlockEntity blockEntity) {
+        if (!(blockEntity instanceof CommandBlockEntity)) {
+            return original;
+        }
+
+        original.remove("Command");
+        return original;
+    }
+}
