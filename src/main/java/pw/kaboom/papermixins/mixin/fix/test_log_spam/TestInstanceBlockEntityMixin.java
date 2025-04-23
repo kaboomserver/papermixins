8ba2c2e9f8b61bd62f550c7c556793eeb2ad7d15
amyavi <144570677+amyavi@users.noreply.github.com>
2025-04-23 10:47:29 -0300

    fix: don't allow exporting tests

---
+++ src/main/java/pw/kaboom/papermixins/mixin/fix/test_log_spam/TestInstanceBlockEntityMixin.java
@@ -0,0 +1,20 @@
+package pw.kaboom.papermixins.mixin.fix.test_log_spam;
+
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.block.entity.TestInstanceBlockEntity;
+import org.spongepowered.asm.mixin.Mixin;
+
+import java.util.function.Consumer;
+
+@Mixin(TestInstanceBlockEntity.class)
+public abstract class TestInstanceBlockEntityMixin {
+    @WrapMethod(method = "export")
+    private static boolean export(final ServerLevel level, final ResourceLocation test,
+                                  final Consumer<Component> messageSender, final Operation<Boolean> original) {
+        return true; // fail
+    }
+}
