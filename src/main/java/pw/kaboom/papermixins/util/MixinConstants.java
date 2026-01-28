89490b8b9889a5c376059154fc23ad6e0c8e76d5
amy <144570677+amyavi@users.noreply.github.com>
2026-01-27 21:24:40 -0300

    refactor: use proper way to skip failing packets

--- src/main/java/pw/kaboom/papermixins/util/MixinConstants.java
+++ src/main/java/pw/kaboom/papermixins/util/MixinConstants.java
@@ -3,6 +3,10 @@ package pw.kaboom.papermixins.util;
 import com.mojang.brigadier.CommandDispatcher;
 import io.papermc.paper.command.brigadier.ApiMirrorRootNode;
 import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.network.protocol.Packet;
+import net.minecraft.network.protocol.game.*;
+
+import java.util.Set;
 
 public final class MixinConstants {
     // Static abuse so we can reference stuff in multiple mixins
@@ -17,6 +21,15 @@ public final class MixinConstants {
                     return MixinConstants.VANILLA_DISPATCHER;
                 }
             });
+
+    public static final Set<Class<? extends Packet<?>>> FAIL_IGNORED_PACKETS = Set.of(
+        ClientboundSetTitleTextPacket.class, ClientboundSetSubtitleTextPacket.class,
+        ClientboundSetActionBarTextPacket.class, ClientboundSetPlayerTeamPacket.class,
+        ClientboundLevelParticlesPacket.class,  ClientboundSetEntityDataPacket.class,
+        ClientboundContainerSetContentPacket.class, ClientboundContainerSetSlotPacket.class,
+        ClientboundLevelChunkWithLightPacket.class, ClientboundBlockEntityDataPacket.class,
+        ClientboundSetPlayerInventoryPacket.class, ClientboundSetObjectivePacket.class
+    );
 
     private MixinConstants() {}
 }
