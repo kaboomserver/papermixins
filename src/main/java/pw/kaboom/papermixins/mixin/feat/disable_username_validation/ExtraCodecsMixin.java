5d52df9d9d0861674b93d7f47a2a293ce1205972
amy <144570677+amyavi@users.noreply.github.com>
2026-06-16 00:10:39 -0300

    style: use WrapOperation instead of Overwrite

--- src/main/java/pw/kaboom/papermixins/mixin/feat/disable_username_validation/ExtraCodecsMixin.java
+++ src/main/java/pw/kaboom/papermixins/mixin/feat/disable_username_validation/ExtraCodecsMixin.java
@@ -1,10 +1,11 @@
 package pw.kaboom.papermixins.mixin.feat.disable_username_validation;
 
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.mojang.serialization.DataResult;
 import net.minecraft.util.ExtraCodecs;
 import net.minecraft.util.StringUtil;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 
 @Mixin(ExtraCodecs.class)
 public abstract class ExtraCodecsMixin {
@@ -12,8 +13,8 @@ public abstract class ExtraCodecsMixin {
      * @author amyavi
      * @reason Filter text instead of throwing error to prevent server crashes
      */
-    @Overwrite
-    private static DataResult<String> lambda$static$63(final String string) {
+    @WrapMethod(method = "lambda$static$63")
+    private static DataResult<String> lambda$static$63(final String string, final Operation<DataResult<String>> original) {
         return DataResult.success(StringUtil.filterText(string));
     }
 }
