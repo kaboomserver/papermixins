f2e7ee6d9b107ee0efeb03e7240409b5e20cb8f4
opt <opt@chipmunk.land>
2026-08-17 03:28:34 +0000

    fix: update lambda id for log spam mixin

--- src/main/java/pw/kaboom/papermixins/pluginmixin/mixins/feat/remove_log_spam/FaweMixin.java
+++ src/main/java/pw/kaboom/papermixins/pluginmixin/mixins/feat/remove_log_spam/FaweMixin.java
@@ -11,7 +11,7 @@ import pw.kaboom.papermixins.pluginmixin.PluginMixin;
 @PluginMixin("FastAsyncWorldEdit")
 @Mixin(Fawe.class)
 public abstract class FaweMixin {
-    @WrapOperation(method = "lambda$setupMemoryListener$2",
+    @WrapOperation(method = "lambda$setupMemoryListener$0",
         at = @At(value = "INVOKE", target = "Lorg/apache/logging/log4j/Logger;warn(Ljava/lang/String;)V"),
         require = 0)
     private static void setupMemoryListener$warnAboutLowMemory(final Logger instance, final String s,
